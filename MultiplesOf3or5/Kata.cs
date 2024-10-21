9ba226079472181206aeda4622a12cd519d19af2
TorLindstrom <tor.lindstrom@winlas.se>
2024-10-21 13:19:40 +0200

    kata: sum of multiples of 3 or 5

---
+++ MultiplesOf3or5/Kata.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+
+namespace MultiplesOf3or5;
+
+public static class Kata
+{
+	public static int Solution(int value) => Enumerable
+			.Range(1, Math.Max(0, value  - 1))
+			.Where(i => i % 3 == 0 || i % 5 == 0)
+			.Sum();
+}
--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -1,15 +1,30 @@
+using MultiplesOf3or5;
+
 namespace Test;
 
 public class Tests
 {
-    [SetUp]
-    public void Setup()
-    {
-    }
-
     [Test]
-    public void Test1()
+    public void NegativeInput_ReturnsZero()
     {
-        Assert.Pass();
+        var result = Kata.Solution(-1);
+        Assert.That(result, Is.EqualTo(0));
+    }
+    
+    [Test]
+    public void OneMultiplePresent_ReturnsMultiple()
+    {
+        var result = Kata.Solution(5);
+        Assert.That(result, Is.EqualTo(3));
+    }
+    
+    [Theory]
+    [TestCase(6, 8)]
+    [TestCase(10, 23)]
+    [TestCase(20, 78)]
+    public void MultipleMultiplesBelowInput_ReturnsSumOfMultiples(int input, int expected)
+    {
+        var result = Kata.Solution(input);
+        Assert.That(result, Is.EqualTo(expected));
     }
 }
